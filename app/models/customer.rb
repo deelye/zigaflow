030af79ab01b9ed18cf3320a7902eb91bc031a09
deelye <dee.yen.lye@gmail.com>
2020-04-25 16:55:12 +0100

    added associations and validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  has_many :invoices
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, uniqueness: true, presence: true
+  validates :email, format: { with: /\A.*@.*\.com\z/i } uniqueness: true, presence: true
 end
