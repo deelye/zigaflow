d9fa57de028a77cec55721c315d0de435e580075
deelye <dee.yen.lye@gmail.com>
2020-04-25 16:59:34 +0100

    added associations and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many :invoices
+
+  validates :name, uniqueness: true, presence: true
+  validates :description, uniqueness: true, presence: true
+  validates :gross, presence: true
 end
