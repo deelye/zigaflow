823cb8077ef7cd4802dab7a2aadf799bd7452fb1
deelye <dee.yen.lye@gmail.com>
2020-04-25 17:44:13 +0100

    added numericality validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
   belongs_to :invoice
   belongs_to :product
 
-  validates :quantity, presence: true
+  validates :quantity, numericality: { only_integer: true }, presence: true
 end
