622710c48c7bfbcd00fbbc3f5fdc806288a1a33b
deelye <dee.yen.lye@gmail.com>
2020-04-26 18:09:25 +0100

    added invoice controller and index, new, show views

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,2 +1,33 @@
 class InvoicesController < ApplicationController
+  def index
+    @invoices = Invoice.all
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def new
+    @customers = Customer.all
+    @invoice = Invoice.new
+  end
+
+
+  def create
+    @customer = Customer.find(invoice_params["customer_id"].to_i)
+    date = invoice_params["date"]
+    @invoice = Invoice.new({ customer_id: @customer.id, date: date})
+    if @invoice.save
+      redirect_to invoice_path(@invoice)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:customer_id, :date)
+  end
+
 end
