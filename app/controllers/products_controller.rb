7ec19694ec2b335e64144eceeb60c033403be664
deelye <dee.yen.lye@gmail.com>
2020-04-26 23:30:04 +0100

    can edit product price only; added extra customer details

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,28 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update]
+
   def index
     @products = Product.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(restaurant_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:product).permit(:gross)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
